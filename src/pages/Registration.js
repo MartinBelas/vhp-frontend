482114d0a1cf1713d1ea90a08ab1544c24b2e0e1
Martin Belas <Martin.Belas@outlook.com>
2021-01-24 21:06:06 +0100

    refactor: registration to functional components

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-class Registration extends Component {
+export default function Registration() {
 
-    state = {registeredRunners: []}
+    const [registeredRunners, setRegisteredRunners] = useState([]);
 
-    componentDidMount() {
+    useEffect(() => {
         const options = {
             headers: {'api-key': process.env.REACT_APP_API_KEY}
         };
@@ -14,35 +14,31 @@ class Registration extends Component {
         axios.get('/api/users', options)
             .then(response  => {
                 if (response.data) {
-                    this.setState({ registeredRunners: response.data });
+                    setRegisteredRunners(response.data);
                 }
             })
             .catch(error => {
                 console.log(error);
             });
-    }
+        }, [])
 
-    render() {
-        return (
-            <div id="content">
-                <h2>REGISTRACE</h2>
-                Chcete se přihlásit?
-                <div>
-                    <b><Link to={'/registracni-formular'} className="nav-link">Přihlášku - on-line formulář najdete zde.</Link></b>
-                </div>
-                
-                <br />
-                <hr />
-                <div>
-                    <h3>Registrováni:</h3>
-                    atd...
-                    {this.state.registeredRunners.map(user =>
-                        <div key={user.id}>{user.firstName}</div>
-                    )}
-                </div>
+    return (
+        <div id="content">
+            <h2>REGISTRACE</h2>
+            Chcete se přihlásit?
+            <div>
+                <b><Link to={'/registracni-formular'} className="nav-link">Přihlášku - on-line formulář najdete zde.</Link></b>
             </div>
-        );
-    }
+            
+            <br />
+            <hr />
+            <div>
+                <h3>Registrováni:</h3>
+                atd...
+                {registeredRunners.map(user =>
+                    <div key={user.id}>{user.firstName}</div>
+                )}
+            </div>
+        </div>
+    );
 }
-
-export default Registration;
--- src/pages/RegistrationForm.js
+++ src/pages/RegistrationForm.js
@@ -1,282 +1,253 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from "@material-ui/core/Button";
 import { TextField } from '@material-ui/core';
 
 import validate from '../services/validator.js'
 
-class SendButton extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isDisabled: true
-		}
-	}
-	
-	updateButtonDisableState(newDisableState) {
-		console.dir('NEW STATE: ' + newDisableState);
-		this.setState({
-			isDisabled: newDisableState
-		})
-	}
-	
-	render() {
-		return ( 
-		  <div>
-			<Button variant="contained" color="primary" type="submit" disabled={this.state.isDisabled}>
-				Odeslat
-			</Button>
-		  </div>
-		);
-	}
-}
-
-class ApplicationForm extends React.Component {
-  constructor(props) {
-    super(props);
-	this.submitButtonElement = React.createRef();
-    this.state = {
-		formIsValid: false,
-		formControls: {
+function ApplicationForm() {
+		
+	const [formIsValid, setFormIsValid] = useState(false);
+	const [formControls, setFormControls] = useState(
+		{
 			name: {
 				value: '',
 				valid: false,
 				validationRules: {
-				  minLength: 2
+					minLength: 2
 				}
-            },
+			},
 			surname: {
-            	value: '',
+				value: '',
 				valid: false,
 				validationRules: {
-				  minLength: 2
+					minLength: 2
 				}
-            },
-            email: {
-            	value: '',
+			},
+			email: {
+				value: '',
 				valid: false,
 				validationRules: {
 					minLength: 6
 				}
-            },
+			},
 			tel: {
-            	value: '',
+				value: '',
 				valid: true,
-            },
+			},
 			year: {
-                value: '',
+				value: '',
 				valid: false,
 				validationRules: {
-						minLength: 4,
-						maxLength: 4,
-						minValue: 1900,
-						maxValue: 2099,
-						isRequired: true
-					}				
-            },
+					minLength: 4,
+					maxLength: 4,
+					minValue: 1900,
+					maxValue: 2099,
+					isRequired: true
+				}
+			},
 			sex: {
-                value: '',
+				value: '',
 				valid: false,
 				validationRules: {
 					isRequired: true,
 				},
 				options: [
 					{ value: 'male', displayValue: 'Muž' },
-					{ value: 'female', displayValue: 'Žena'}
+					{ value: 'female', displayValue: 'Žena' }
 				]
-            },
-            home: {
-                value: '',
+			},
+			home: {
+				value: '',
 				valid: false,
 				validationRules: {
 					isRequired: true,
 					minLength: 2
 				}
-            },
+			},
 			club: {
-            	value: '',
+				value: '',
 				valid: true,
-            },
+			},
 			race: {
-            	value: '',
+				value: '',
 				valid: false,
 				validationRules: {
 					isRequired: true,
 				},
-				options: [
+				options: [ //TODO generate from BE according to administration
 					{ value: 'galerijni', displayValue: 'Běh 3,6 km Galerijní ulicí' },
-					{ value: 'ctvrtmaraton', displayValue: '1/4 maratón'},
-					{ value: 'pulmaraton', displayValue: '1/2 maratón'},
-					{ value: 'maraton', displayValue: 'Maratón'}
+					{ value: 'ctvrtmaraton', displayValue: '1/4 maratón' },
+					{ value: 'pulmaraton', displayValue: '1/2 maratón' },
+					{ value: 'maraton', displayValue: 'Maratón' }
 				]
 			},
 			notes: {
-            	value: '',
+				value: '',
 				valid: true,
-            }
+			}
 		}
-	};
+	);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+	function handleChange(event) {
+		const name = event.target.name;
+		const value = event.target.value;
 
-  handleChange(event) {
-    const name = event.target.name;
-    const value = event.target.value;
+		const updatedControls = {
+			...formControls
+		};
+		const updatedFormElement = {
+			...updatedControls[name]
+		};
+		updatedFormElement.value = value;
+		updatedFormElement.valid = validate(value, updatedFormElement.validationRules);
 
-	const updatedControls = {
-		...this.state.formControls
-	};
-	const updatedFormElement = {
-		...updatedControls[name]
-	};
-	updatedFormElement.value = value;
-	updatedFormElement.touched = true;
-	updatedFormElement.valid = validate(value, updatedFormElement.validationRules);
+		updatedControls[name] = updatedFormElement;
 
-	updatedControls[name] = updatedFormElement;
+		let formIsValid = true;
+		for (let inputIdentifier in updatedControls) {
+			formIsValid = updatedControls[inputIdentifier].valid && formIsValid;
+		}
 
-	let formIsValid = true;
-	for (let inputIdentifier in updatedControls) {
-		formIsValid = updatedControls[inputIdentifier].valid && formIsValid;
+		setFormControls(updatedControls);
+		setFormIsValid(formIsValid);
+
+		console.dir('VALID: ' + formIsValid);
 	}
 
-	this.setState({
-		formControls: updatedControls,
-		formIsValid: formIsValid
-	});
-
-	console.dir('VALID: ' + this.state.formIsValid);
-	this.submitButtonElement.current.updateButtonDisableState(!this.state.formIsValid);
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.formControls.name.value);
-	alert('STATE: ' + this.state.formIsValid);
-	console.dir(this.state.formControls);
-    event.preventDefault();
-	const data = new FormData(event.target);
-    
-    fetch('/api/form-submit-url', {
-      method: 'POST',
-      body: data,
-    });
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-  
-		<fieldset>
-		<legend>Identifikace závodníka</legend>
-			<TextField name="name" label="Jméno" variant="outlined" margin="dense"
-				required
-				value={this.state.formControls.name.value} 
-				onChange={this.handleChange} 
-				//touched={this.state.formControls.name.touched}
-				//valid={this.state.formControls.name.valid}
-				error={!this.state.formControls.name.valid}
-			/>
-			&nbsp; 
-			<TextField name="surname" label="Příjmení" variant="outlined" margin="dense"
-				required
-				value={this.state.formControls.surname.value} 
-				onChange={this.handleChange} 
-				//touched={this.state.formControls.surname.touched}
-				//valid={this.state.formControls.surname.valid}
-				error={!this.state.formControls.surname.valid}
-			/>
-			
-			<br />
-			<TextField name="email" label="E-mail" variant="outlined" margin="dense"
-				type="email" required
-				value={this.state.formControls.email.value} 
-				onChange={this.handleChange} 
-				error={!this.state.formControls.email.valid}
-			/>
-			&nbsp; 	
-			<TextField id="tel" label="Telefon" variant="outlined" margin="dense"
-				name="tel" 
-				value={this.state.formControls.tel.value} 
-				onChange={this.handleChange} 
-			/>
-				
-			<br />	
-			<TextField name="sex" label="Pohlaví" variant="outlined" margin="dense"
-				select required style={{minWidth: 120}}
-				options={this.state.formControls.sex.options}
-				value={this.state.formControls.sex.value} 
-				onChange={this.handleChange} 
-				error={!this.state.formControls.sex.valid} >
-					<option value="M">&nbsp; Muž </option>
-  					<option value="F">&nbsp; Žena </option>
-			</TextField>
-			&nbsp;  &nbsp;  
-			<TextField name="year" label="Rok naroz." variant="outlined" margin="dense"
-				type="number" required
-				value={this.state.formControls.year.value} 
-				onChange={this.handleChange} 
-				error={!this.state.formControls.year.valid}
-			/>
-				
-			<br />
-			<TextField name="home" label="Město/obec" variant="outlined" margin="dense"
-				required 
-				value={this.state.formControls.home.value} 
-				onChange={this.handleChange} 
-				error={!this.state.formControls.home.valid}
-			/>
-			&nbsp; 	
-			<TextField name="club" label="Klub" variant="outlined" margin="dense"
-				value={this.state.formControls.club.value} 
-				onChange={this.handleChange} />
-
-		</fieldset>
+	function handleSubmit(event) {
+		event.preventDefault();
 		
-		<br />
-		<fieldset>
-		<legend>Závod</legend>
-			<TextField name="race" label="Závod" variant="outlined" margin="dense" select  style={{minWidth: 250}}
-					value={this.state.formControls.race.value} 
-					onChange={this.handleChange} >
-						<option value="galerijni">&nbsp;  Běh 3,6 km Galerijní ulicí</option>
-  						<option value="ctvrtmaraton">&nbsp;  1/4 maratón</option>
-						<option value="pulmaraton">&nbsp;  1/2 maratón</option>
-  						<option value="maraton">&nbsp;  Maratón</option>
-			</TextField>	
-		</fieldset>
-        
-		<br />  
-        <fieldset>        
-        <legend>Poznámky </legend>
-			<TextField name="notes" label="Poznámky" variant="outlined" margin="dense" multiline
-				value={this.state.formControls.notes.value} 
-				onChange={this.handleChange} />
-        </fieldset>
-        
-		<br />
-		<hr />
-        <br />
-		<div style={{textAlign:'center'}}>
-			<SendButton ref={this.submitButtonElement} />
-		</div>
+		let payload = new Map();
+		payload['name'] = formControls.name.value;
+		payload['surname'] = (formControls.surname.value);
+		payload['email'] = formControls.email.value;
+		payload['tel'] = formControls.tel.value;
+		payload['sex'] = formControls.sex.value;
+		payload['year'] = formControls.year.value;
+		payload['home'] = formControls.home.value;
+		payload['club'] = formControls.club.value;
+		payload['race'] = formControls.race.value;
+		payload['notes'] = formControls.notes.value;
 
-      </form>
-    );
-  }
+		payload = JSON.stringify(payload);
+		//console.dir(" --> PAYLOAD : ", payload); //TODO delete
+
+		fetch('/api/form-submit-url', {
+			method: 'POST',
+			body: payload,
+		});
+	}
+
+	return (
+		<form onSubmit={e => {handleSubmit(e)}}>
+
+			<fieldset>
+				<legend>Identifikace závodníka</legend>
+				<TextField name="name" label="Jméno" variant="outlined" margin="dense"
+					required
+					value={formControls.name.value}
+					onChange={e => handleChange(e)}
+					error={!formControls.name.valid}
+				/>
+			&nbsp;
+			<TextField name="surname" label="Příjmení" variant="outlined" margin="dense"
+					required
+					value={formControls.surname.value}
+					onChange={e => handleChange(e)}
+					error={!formControls.surname.valid}
+				/>
+
+				<br />
+				<TextField name="email" label="E-mail" variant="outlined" margin="dense"
+					type="email" required
+					value={formControls.email.value}
+					onChange={e => handleChange(e)}
+					error={!formControls.email.valid}
+				/>
+			&nbsp;
+			<TextField id="tel" label="Telefon" variant="outlined" margin="dense"
+					name="tel"
+					value={formControls.tel.value}
+					onChange={e => handleChange(e)}
+				/>
+
+				<br />
+				<TextField name="sex" label="Pohlaví" variant="outlined" margin="dense"
+					select required style={{ minWidth: 120 }}
+					options={formControls.sex.options}
+					value={formControls.sex.value}
+					onChange={e => handleChange(e)}
+					error={!formControls.sex.valid} >
+						<option value="M">&nbsp; Muž </option>
+						<option value="F">&nbsp; Žena </option>
+				</TextField>
+			&nbsp;  &nbsp;
+			<TextField name="year" label="Rok naroz." variant="outlined" margin="dense"
+					type="number" required
+					value={formControls.year.value}
+					onChange={e => handleChange(e)}
+					error={!formControls.year.valid}
+				/>
+
+				<br />
+				<TextField name="home" label="Město/obec" variant="outlined" margin="dense"
+					required
+					value={formControls.home.value}
+					onChange={e => handleChange(e)}
+					error={!formControls.home.valid}
+				/>
+			&nbsp;
+			<TextField name="club" label="Klub" variant="outlined" margin="dense"
+					value={formControls.club.value}
+					onChange={e => handleChange(e)} />
+
+			</fieldset>
+
+			<br />
+			<fieldset>
+				<legend>Závod</legend>
+				<TextField name="race" label="Závod" variant="outlined" margin="dense" required select style={{ minWidth: 250 }}
+					value={formControls.race.value}
+					onChange={e => handleChange(e)} >
+						//TODO generate from BE according to administration
+						<option value="galerijni">&nbsp;  Běh 3,6 km Galerijní ulicí</option>
+						<option value="ctvrtmaraton">&nbsp;  1/4 maratón</option>
+						<option value="pulmaraton">&nbsp;  1/2 maratón</option>
+						<option value="maraton">&nbsp;  Maratón</option>
+				</TextField>
+			</fieldset>
+
+			<br />
+			<fieldset>
+				<legend>Poznámky </legend>
+				<TextField name="notes" label="Poznámky" variant="outlined" margin="dense" multiline
+					value={formControls.notes.value}
+					onChange={e => handleChange(e)} />
+			</fieldset>
+
+			<br />
+			<hr />
+			<br />
+
+			<div style={{ textAlign: 'center' }}>
+				<Button variant="contained" color="primary" type="submit" disabled={!formIsValid}>
+					Odeslat
+				</Button>
+			</div>
+
+		</form>
+	);
 }
 
-class RegistrationForm extends Component {
-  render() {
-    return (
-        <div id="content">
+function RegistrationForm() {
+	return (
+		<div id="content">
 			<h2>REGISTRACE / PŘIHLÁŠKA</h2>
 			<p><a href="/registrace">← zpět na registrace</a></p>
-			
+
 			<ApplicationForm />
-        </div>
-    );
-  }
+		</div>
+	);
 }
 
 export default RegistrationForm;
