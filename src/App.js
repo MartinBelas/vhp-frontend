5dc5a08041722c687dea7eb44c810c9cd498728b
Martin Belas <Martin.Belas@outlook.com>
2021-02-28 21:14:30 +0100

    fix: typo

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
                 }
             })
             .catch(err => {
-                console.log('app err: ', err.mesage);
+                console.log('app err: ', err.message);
             })
     }, []);
 
