cbf0bf3fb3a014f8304dcdad104498ea9e34a65b
Martin Belas <Martin.Belas@outlook.com>
2021-03-07 20:21:34 +0100

    feat: add Export link

--- src/pages/Administration.js
+++ src/pages/Administration.js
@@ -13,9 +13,13 @@ export default function Administration() {
                 <h2>ADMINISTRACE</h2>
                 <br/>
                 <ul>
-                    <li><NavLink to='/adm/novy-rocnik' className="nav-link">Nový&nbsp;ročník</NavLink></li><br/>
-                    <li><NavLink to='/adm/novinky' className="nav-link">Novinky</NavLink></li><br/>
+                    <li><NavLink to='/adm/novy-rocnik' className="nav-link">Nový&nbsp;ročník</NavLink></li>
+                    <br/>
+                    <li><NavLink to='/adm/novinky' className="nav-link">Novinky</NavLink></li>
+                    <br/>
                     <li><NavLink to='/adm/registrace' className="nav-link">Registrace</NavLink></li>
+                    <br/>
+                    <li><NavLink to='/adm/export' className="nav-link">Export registrací</NavLink></li>
                 </ul>
             </div>
             : <Redirect to="/" />
